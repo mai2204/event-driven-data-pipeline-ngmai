aa73f1cde708a2c6da236196c95aa4939128642b
mai2204 <ngmai7448@gmail.com>
2026-04-28 16:13:36 +0700

    add aws_creds into s3_helper

--- utils/s3_helper.py
+++ utils/s3_helper.py
@@ -1,19 +1,20 @@
 import boto3
+from prefect_aws import AwsCredentials
 
-s3 = boto3.client("s3")
-
+aws_creds = AwsCredentials.load("aws-credentials")
+s3_client = aws_creds.get_boto3_session().client("s3")
 def list_files(bucket, s3_client):
     response = s3_client.list_objects_v2(Bucket=bucket)
     return [obj["Key"] for obj in response.get("Contents", [])]
 
 def move_file(source_bucket, key, target_bucket, target_key):
-    s3.copy_object(
+    s3_client.copy_object(
         Bucket=target_bucket,
         CopySource={'Bucket': source_bucket, 'Key': key},
         Key=target_key
     )
 
-    s3.delete_object(
+    s3_client.delete_object(
         Bucket=source_bucket,
         Key=key
     )
